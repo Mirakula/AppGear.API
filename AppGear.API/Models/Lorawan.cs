0d979c3e2c5272ec58d52f32fd561cfdf6008ab8
Amar Džanan <amar.dzanan@edu.fit.ba>
2020-11-06 21:56:56 +0100

    Objekat Lorawan, postavljeni property.

--- AppGear.API/Models/Lorawan.cs
+++ AppGear.API/Models/Lorawan.cs
@@ -2,6 +2,8 @@
 {
     public class Lorawan
     {
-        
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int  Counter { get; set; }
     }
 }
