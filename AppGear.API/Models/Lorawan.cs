b61ac86a581aea99fa2acf0b18945996673d10e5
Amar Džanan <amar.dzanan@edu.fit.ba>
2020-11-06 22:03:31 +0100

    Lorawan klasa priprema za bazu.

--- AppGear.API/Models/Lorawan.cs
+++ AppGear.API/Models/Lorawan.cs
@@ -1,9 +1,26 @@
-﻿namespace AppGear.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppGear.API.Models
 {
+    [Table(("Lorawan"))]
     public class Lorawan
     {
+        [Column("LorawanId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("Longitude")]
         public double Longitude { get; set; }
+
+        [Column("Latitude")]
         public double Latitude { get; set; }
+
+        [Column("Counter")]
         public int  Counter { get; set; }
+        
+        [Column("IpAddress")]
+        public string IpAddress { get; set; }
     }
 }
