3f5f7fec3e8734916d658fb7a70297ec15c386fa
Amar Džanan <amar.dzanan@edu.fit.ba>
2020-11-23 16:46:50 +0100

    Dodani Endpointi za testiranje IoT i Endpoint za produkciju. Dodane dvije tabele u ORM za komunikaciju

--- AppGear.API/Controllers/LorawanController.cs
+++ AppGear.API/Controllers/LorawanController.cs
@@ -1,7 +1,13 @@
-﻿namespace AppGear.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppGear.API.Controllers
 {
-    public class LorawanController
+    public class LorawanController : Controller
     {
-        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
