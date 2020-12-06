d3b0b9f9edb3687727c3f44fd783c14f97c81ce9
Amar Džanan <amar.dzanan@edu.fit.ba>
2020-12-06 19:35:41 +0100

    Dodan dekoder koji dekodira podatke iz Tabela kolona data u citljive podatke sa IoT Senzora

---
+++ AppGear.API/Controllers/LoriotDecoderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppGear.API.Controllers
+{
+    public class LoriotDecoder : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
